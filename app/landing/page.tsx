07b5c278fa12cefb2a064f99a69696abf35f817c
Ria Talsania <riatalsania0510@gmail.com>
2025-04-20 01:23:27 +0530

    landinf basic

---
+++ app/landing/page.tsx
@@ -0,0 +1,12 @@
+// landing/page.tsx
+// "use client"
+
+import SplineModel from "./spline";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <SplineModel />
+    </main>
+  );
+}
---
+++ app/landing/spline.tsx
@@ -0,0 +1,13 @@
+
+
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/b5tnFzEqaI08PdoC/scene.splinecode" 
+      />
+    </main>
+  );
+}
