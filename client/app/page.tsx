ac6835ba9db1aed1a2cfcc62f0b1954ed1a8d225
Ria Talsania <riatalsania0510@gmail.com>
2025-04-20 10:47:11 +0530

    finally ria comitted

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,17 +1,38 @@
-import Header from './Header'
-import Footer from './Footer'
-export default function HomePage() {
+'use client'
+
+import { useState } from 'react'
+import SplineModel from '../app/landing/spline'
+import Home from '../app/landing/home'
+import CreateButton from '../app/landing/create'
+
+export default function LandingPage() {
+  const [isSplineLoaded, setIsSplineLoaded] = useState(false)
+
+  const handleSplineLoad = () => {
+    setIsSplineLoaded(true)
+  }
+
   return (
-    <main>
-      <Header />
-      <section className="p-8">
-        <h2 className="text-xl">Welcome to the homepage!</h2>
-        {/* More content here */}
-      </section>
-      <Footer/>
+    <main className="relative min-h-screen w-full overflow-x-hidden">
+      {!isSplineLoaded && (
+        <div className="fixed top-0 left-0 h-screen w-screen bg-[#598bd9] z-50" />
+      )}
+
+      <div className="h-screen w-full relative z-40">
+        <SplineModel onLoaded={handleSplineLoad} />
+      </div>
+
+      {isSplineLoaded && (
+        <>
+          <div className="relative z-30">
+            <Home />
+          </div>
+
+          <div className="relative z-30 -mt-10">
+            <CreateButton />
+          </div>
+        </>
+      )}
     </main>
-    
   )
 }
-
-
